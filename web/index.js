80fb402e916a15885d87c7c48e5043845eb6c68e
Ganesh U <ganesh.u@bookmyshow.com>
2019-08-25 01:54:46 +0530

    error handling and tile generation

--- web/index.js
+++ web/index.js
@@ -7,8 +7,8 @@ const ctx = canvas.getContext('2d');
 // image to recieve the image uploaded by user
 const img = new Image();
 const fileInput = document.getElementById("file-input");
-const TILE_WIDTH = 16;
-const TILE_HEIGHT = 16;
+const TILE_WIDTH = 60;
+const TILE_HEIGHT = 60;
 
 // listen to file upload
 fileInput.onchange = imageUpload;
@@ -35,7 +35,7 @@ function imageUpload() {
 img.onload = () => {
     canvas.width = img.width;
     canvas.height = img.height;
-    ctx.drawImage(img, 0, 0);
+    ctx.drawImage(img,0,0);
     let dataUri = canvas.toDataURL();
     dataUri = dataUri.replace('data:image/png;base64,', '');
 
@@ -43,12 +43,26 @@ img.onload = () => {
     const tiles = generateTiles();
     console.timeEnd("generating tiles");
 
-    const palette = get_color_palette(dataUri);
+   const palette = get_color_palette(dataUri);
+   showColorPalette(palette);
+    
     const colors = mosaify(tiles);
-    console.log('=======================colors============================');
-    console.log(colors);
-    console.log('=======================colors============================');
-    showColorPalette(palette);
+    drawMosaic(colors);
+    
+    
+}
+
+function drawMosaic(colors) {
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    colors.forEach(data => {
+        const {color, x, y} = data
+        ctx.fillStyle = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+        ctx.beginPath();
+        //ctx.arc(x, y, TILE_WIDTH/2, 0, 2 * Math.PI);
+        //ctx.fill();
+        ctx.fillRect(x,y,TILE_WIDTH, TILE_HEIGHT);
+    })
+    
 }
 
 /**
@@ -70,15 +84,25 @@ const showColorPalette = (colors) => {
  * generate tiles of image data with x, y co-ordinates
  */
 function generateTiles() {
-    const rows = img.width / TILE_WIDTH;
-    const cols = img.height / TILE_HEIGHT;
+    const cols = img.width / TILE_WIDTH;
+    const rows = img.height / TILE_HEIGHT;
+    console.log("log: -------------------------------")
+    console.log("log: generateTiles -> cols", Math.floor(cols))
+    console.log("log: -------------------------------")
+    console.log("log: -------------------------------")
+    console.log("log: generateTiles -> rows", Math.floor(rows))
+    console.log("log: -------------------------------")
+    
     const imageTiles = [];
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    document.body.appendChild(canvas);
     for (let y = 0; y < rows; ++y) {
         for (let x = 0; x < cols; ++x) {
-            const canvas = document.createElement('canvas');
+            
             canvas.width = TILE_WIDTH;
             canvas.height = TILE_HEIGHT;
-            const context = canvas.getContext('2d');
+            
 
             context.drawImage(
                 img,
@@ -100,6 +124,7 @@ function generateTiles() {
                 "x": x * TILE_WIDTH,
                 "y": y * TILE_HEIGHT
             });
+            //context.clearRect(0, 0, TILE_WIDTH, TILE_HEIGHT);
         } //end of colums for loop
     } //end of row for loop
     return imageTiles;
